cdf585d02b2dcf3bb35234647b20c0aa5b069bfb
Phongcntt <phongqv11a1@gmail.com>
2025-09-19 23:21:45 +0700

    Push de lm UI

--- Assets/Scripts/Phong/Hero/FlightBullet.cs
+++ Assets/Scripts/Phong/Hero/FlightBullet.cs
@@ -1,11 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class FlightBullet : MonoBehaviour
 {
     public float damage = 50f;
     public GameObject boomPrefab;
+
+    private void Start()
+    {
+        DOVirtual.DelayedCall(3f, () => { Destroy(gameObject); });
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {

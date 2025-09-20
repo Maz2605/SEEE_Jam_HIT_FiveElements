220de02ad68a45869acb5fd4d26a5877f8a61a80
AcXuan <xuanphucit7575@gmail.com>
2025-09-20 23:40:55 +0700

    Add pause and continue , fix layer enemy

--- Assets/Scripts/Xuan/Test1.cs
+++ Assets/Scripts/Xuan/Test1.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class Test1 : MonoBehaviour
 {
     [SerializeField] private Button btn;
+    [SerializeField] private Button btnContinue;
+    [SerializeField] private Button btnPause;
 
     private void Start()
     {
@@ -15,5 +17,16 @@ public class Test1 : MonoBehaviour
             Debug.Log("Start Level 1");
             XuanEventManager.OnStartLevel(1);
         });
+        btnContinue.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1;
+            btnContinue.gameObject.SetActive(false);
+        });
+        btnPause.onClick.AddListener(() =>
+        {
+            Time.timeScale = 0;
+            btnContinue.gameObject.SetActive(true);
+        });
     }
+
 }
